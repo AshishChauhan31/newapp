ddcb68cc59541d0b4fac150ac43e8b59215fc20c
Ashish <ashish.chauhan_mca18@gla.ac.in>
2019-03-10 22:44:43 +0530

    media player code applied

--- app/src/main/java/com/example/akhilesh/timerapp/MainActivity.java
+++ app/src/main/java/com/example/akhilesh/timerapp/MainActivity.java
@@ -1,41 +1,71 @@
 package com.example.akhilesh.timerapp;
 
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     Button button;
-    Timer timer;
-   // int time = 100000;
-    //private boolean running;
-
+    public int counter;
+    private MediaPlayer mediaPlayer;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
         textView = findViewById (R.id.textView);
         button = findViewById (R.id.button);
+        button2 = findViewById (R.id.button2);
+
+//       mediaPlayer = MediaPlayer.create (this,R.raw.a);
         button.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View v) {
-                new timer (100000) {
+
+                new CountDownTimer(1000,10) {
                     public void onTick(long millisUntilFinished) {
-                        int counter=0;
+
                         textView.setText (String.valueOf (counter));
                         counter++;
+                        //Toast.makeText (MainActivity.this, "after time song starting", Toast.LENGTH_SHORT).show ();
                     }
 
                     public void onFinish() {
-                        textView.setText ("finish!!!!!");
-                    }
-                };
+                        MediaPlayer mediaPlayer= MediaPlayer.create(MainActivity.this , R.raw.abc);
+//                        mediaPlayer = MediaPlayer.create(null, R.raw.a);
+                        try {
+                            mediaPlayer.prepare();
+                        } catch (IllegalStateException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
 
+                        textView.setText ("hi!");
+                        mediaPlayer.start ();
+                    }
+                }.start ();
             }
+            });
+
+        button2.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText (MainActivity.this, "after stop timer", Toast.LENGTH_SHORT).show ();
+                mediaPlayer.stop ();
+            }
+
         });
     }}
